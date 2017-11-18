be7b2423cef98adf1f3fd2e446ce223455bbfedf
wzf383 <383225639@qq.com>
2017-11-18 14:44:27 +0800

    新增python文件

---
+++ untitled1/haha.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+def calcu_tri(x,y):
+    print "钒哥送心灵鸡汤咯"
+    return 1.0*x*y/2.0
+print "想要吗"
+a=0
+h=0
+area=0.0
+a=input("a=")
+h=input("h=")
+area=calcu_tri(a,h)
+print "Area is"+`area`
